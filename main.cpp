da8385cc0c134904f72c59d99bcf6672e785c0d0
zaharpr <71068575+zaharpr@users.noreply.github.com>
2021-10-10 23:35:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,12 +29,12 @@ void errMessage2() {
 
 //------------------------------------------------------------------------------
 int main(int argc, char *argv[]) {
-//    if (argc != 5) {
-//        errMessage1();
-//        return 1;
-//    }
-//
-//    cout << "Start" << endl;
+    if (argc != 5) {
+        errMessage1();
+        return 1;
+    }
+
+    cout << "Start" << endl;
 
     container c;
     Init(c);
@@ -43,40 +43,40 @@ int main(int argc, char *argv[]) {
     std::ofstream ofstr("..\\test.txt");
     Out(c, ofstr);
 
-//    ////cout << "argv[1] = " << argv[1] << "\n";
-//    if (!strcmp(argv[1], "-f")) {
-//        ifstream ifst(argv[2]);
-//        In(c, ifst);
-//    } else if (!strcmp(argv[1], "-n")) {
-//        auto size = atoi(argv[2]);
-//        if ((size < 1) || (size > 10000)) {
-//            cout << "incorrect number of figures = "
-//                 << size
-//                 << ". Set 0 < number <= 10000\n";
-//            return 3;
-//        }
-//        // системные часы в качестве инициализатора
-//        srand(static_cast<unsigned int>(time(0)));
-//        // Заполнение контейнера генератором случайных чисел
-//        InRnd(c, size);
-//    } else {
-//        errMessage2();
-//        return 2;
-//    }
-//
-//    // Вывод содержимого контейнера в файл
-//    ofstream ofst1(argv[3]);
-//    ofst1 << "Filled container:\n";
-//    Out(c, ofst1);
-//
-//    // The 2nd part of task
-//    ShellSort(c, c.len);
-//    ofstream ofst2(argv[4]);
-//    ofst2 << "SurfaceArea sum = " << SurfaceAreaSum(c) << "\n";
-//    Out(c, ofst2);
-//
-//    Clear(c);
-//    cout << "Stop" << endl;
+    ////cout << "argv[1] = " << argv[1] << "\n";
+    if (!strcmp(argv[1], "-f")) {
+        ifstream ifst(argv[2]);
+        In(c, ifst);
+    } else if (!strcmp(argv[1], "-n")) {
+        auto size = atoi(argv[2]);
+        if ((size < 1) || (size > 10000)) {
+            cout << "incorrect number of figures = "
+                 << size
+                 << ". Set 0 < number <= 10000\n";
+            return 3;
+        }
+        // системные часы в качестве инициализатора
+        srand(static_cast<unsigned int>(time(0)));
+        // Заполнение контейнера генератором случайных чисел
+        InRnd(c, size);
+    } else {
+        errMessage2();
+        return 2;
+    }
+
+    // Вывод содержимого контейнера в файл
+    ofstream ofst1(argv[3]);
+    ofst1 << "Filled container:\n";
+    Out(c, ofst1);
+
+    // The 2nd part of task
+    ShellSort(c, c.len);
+    ofstream ofst2(argv[4]);
+    ofst2 << "SurfaceArea sum = " << SurfaceAreaSum(c) << "\n";
+    Out(c, ofst2);
+
+    Clear(c);
+    cout << "Stop" << endl;
 
     return 0;
 }
