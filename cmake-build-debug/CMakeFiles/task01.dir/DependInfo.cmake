0ab6d2e6322ea2b0593d70d50047983348aefa1a
zaharpr <71068575+zaharpr@users.noreply.github.com>
2021-10-10 22:36:57 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/task01.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/zpris/CLionProjects/01-proc-c++/container.cpp" "C:/Users/zpris/CLionProjects/01-proc-c++/cmake-build-debug/CMakeFiles/task01.dir/container.cpp.obj"
+  "C:/Users/zpris/CLionProjects/01-proc-c++/main.cpp" "C:/Users/zpris/CLionProjects/01-proc-c++/cmake-build-debug/CMakeFiles/task01.dir/main.cpp.obj"
+  "C:/Users/zpris/CLionProjects/01-proc-c++/parallelepiped.cpp" "C:/Users/zpris/CLionProjects/01-proc-c++/cmake-build-debug/CMakeFiles/task01.dir/parallelepiped.cpp.obj"
+  "C:/Users/zpris/CLionProjects/01-proc-c++/rndGenerator.cpp" "C:/Users/zpris/CLionProjects/01-proc-c++/cmake-build-debug/CMakeFiles/task01.dir/rndGenerator.cpp.obj"
+  "C:/Users/zpris/CLionProjects/01-proc-c++/shape.cpp" "C:/Users/zpris/CLionProjects/01-proc-c++/cmake-build-debug/CMakeFiles/task01.dir/shape.cpp.obj"
+  "C:/Users/zpris/CLionProjects/01-proc-c++/sphere.cpp" "C:/Users/zpris/CLionProjects/01-proc-c++/cmake-build-debug/CMakeFiles/task01.dir/sphere.cpp.obj"
+  "C:/Users/zpris/CLionProjects/01-proc-c++/tetrahedron.cpp" "C:/Users/zpris/CLionProjects/01-proc-c++/cmake-build-debug/CMakeFiles/task01.dir/tetrahedron.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
