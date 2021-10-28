648f5c63f611afeee9453627a31ff63d0f920184
zaharpr <71068575+zaharpr@users.noreply.github.com>
2021-10-29 01:06:47 +0300

    Add files via upload

---
+++ AVS-hw2/cmake-build-debug-wsl/CMakeFiles/avs2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/container.cpp" "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/cmake-build-debug-wsl/CMakeFiles/avs2.dir/container.cpp.o"
+  "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/figures/parallelepiped.cpp" "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/cmake-build-debug-wsl/CMakeFiles/avs2.dir/figures/parallelepiped.cpp.o"
+  "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/figures/shape.cpp" "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/cmake-build-debug-wsl/CMakeFiles/avs2.dir/figures/shape.cpp.o"
+  "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/figures/sphere.cpp" "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/cmake-build-debug-wsl/CMakeFiles/avs2.dir/figures/sphere.cpp.o"
+  "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/figures/tetrahedron.cpp" "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/cmake-build-debug-wsl/CMakeFiles/avs2.dir/figures/tetrahedron.cpp.o"
+  "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/main.cpp" "/mnt/c/Users/zpris/CLionProjects/AVS-hw2/cmake-build-debug-wsl/CMakeFiles/avs2.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AVS-hw2/cmake-build-debug-wsl/CMakeFiles/avs2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/avs2"
+  "../bin/avs2.pdb"
+  "CMakeFiles/avs2.dir/container.cpp.o"
+  "CMakeFiles/avs2.dir/figures/parallelepiped.cpp.o"
+  "CMakeFiles/avs2.dir/figures/shape.cpp.o"
+  "CMakeFiles/avs2.dir/figures/sphere.cpp.o"
+  "CMakeFiles/avs2.dir/figures/tetrahedron.cpp.o"
+  "CMakeFiles/avs2.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avs2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
